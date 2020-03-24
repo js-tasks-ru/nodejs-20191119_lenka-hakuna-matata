f140bf0c52193f321a5e0c902455a92e35c27897
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-03-24 15:02:59 +0100

    Added solution for task: Работа с пользователем, сессии

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -1,16 +1,17 @@
+const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const Session = require('./models/Session');
 const uuid = require('uuid/v4');
 const handleMongooseValidationError = require('./libs/validationErrors');
 const mustBeAuthenticated = require('./libs/mustBeAuthenticated');
-const {productsBySubcategory, productList, productById} = require('./controllers/products');
-const {categoryList} = require('./controllers/categories');
 const {login} = require('./controllers/login');
 const {oauth, oauthCallback} = require('./controllers/oauth');
 const {me} = require('./controllers/me');
 
 const app = new Koa();
+
+app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 app.use(async (ctx, next) => {
@@ -31,10 +32,11 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
-
+    await Session.create({token, user, lastVisit: new Date()});
+    
     return token;
   };
-
+  
   return next();
 });
 
@@ -43,21 +45,39 @@ const router = new Router({prefix: '/api'});
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
-
+  
+  const token = header.split(' ')[1];
+  if (!token) return next();
+  
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+  session.lastVisit = new Date();
+  await session.save();
+  
+  ctx.user = session.user;
   return next();
 });
-
-router.get('/categories', categoryList);
-router.get('/products', productsBySubcategory, productList);
-router.get('/products/:id', productById);
 
 router.post('/login', login);
 
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
+
+// this for HTML5 history in browser
+const fs = require('fs');
+
+const index = fs.readFileSync(path.join(__dirname, 'public/index.html'));
+app.use(async (ctx) => {
+  if (ctx.url.startsWith('/api') || ctx.method !== 'GET') return;
+  
+  ctx.set('content-type', 'text/html');
+  ctx.body = index;
+});
 
 module.exports = app;
